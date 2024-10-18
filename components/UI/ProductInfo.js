0cd64b4bb2b5a3a80feebd925c04a0bfabf824fb
Emir Muhammet Aydemir <emirmaydemir@gmail.com>
2024-10-19 00:36:52 +0300

    tresto

--- components/UI/ProductInfo.js
+++ components/UI/ProductInfo.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const ProductInfo = ({ singleProductItem }) => {
   return (
-    <div className="product__info">
-      <h2 className="section__title">{singleProductItem.name}</h2>
-      <p className="section__description">{singleProductItem.description}</p>
+    <div className="product__info2">
+      <h2 className="section__title2">{singleProductItem.name}</h2>
+      <p className="section__description2">{singleProductItem.description}</p>
     </div>
   );
 };
