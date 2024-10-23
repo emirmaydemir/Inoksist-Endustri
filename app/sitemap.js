d308c5ebf9721941d0b0c86b6e140b092327d3e5
Emir Muhammet Aydemir <emirmaydemir@gmail.com>
2024-10-23 11:15:28 +0300

    sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,7 +4,7 @@ import path from "path";
 //export const revalidate = 60;
 
 export default function sitemap() {
-  const baseUrl = "https://www.inoksist.com.tr"; // Burayı canlı URL ile değiştirin
+  const baseUrl = "https://inoksist.com.tr"; // Burayı canlı URL ile değiştirin
 
   // JSON dosyasının yolunu belirtin
   const filePath = path.join(process.cwd(), "locales", "tr", "allData.json");
